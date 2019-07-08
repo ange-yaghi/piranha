317add62f651c38d7426226c950a66d04660d571
Ange Yaghi <ange.yaghi@outlook.com>
2019-07-07 21:56:49 -0400

    [#1] : Upgraded build scripts

--- scripts/compile_flex_bison.bat
+++ scripts/compile_flex_bison.bat
@@ -1,3 +1,5 @@
+@echo off
+
 set PATH=%PATH%;%~dp0/../dependencies/tools/bison/bin;%~dp0/../dependencies/tools/flex/bin
 if not exist "%~dp0/../autogen/" (
     mkdir "%~dp0/../autogen/"
@@ -5,3 +7,5 @@ if not exist "%~dp0/../autogen/" (
 
 win_bison.exe -Wconflicts-sr --verbose "%~dp0/../flex-bison/specification.y" --defines="%~dp0/../autogen/parser.auto.h" --output="%~dp0/../autogen/parser.auto.cpp"
 win_flex.exe -+ -wincompat -o"%~dp0/../autogen/scanner.auto.cpp" "%~dp0/../flex-bison/scanner.l"
+
+@echo on
